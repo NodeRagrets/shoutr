01241c2d0ce5245542856973547bcb50372d7653
tbergen1 <tom@servant.co>
2015-12-17 12:46:49 -0800

    [Feature] added routing for angular views

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -4,6 +4,34 @@ angular.module('shoutr',[
   'shoutr.auth',
   'shoutr.newsFeed'
 ])
-.config([function(){
+.config([function($stateProvider, $urlRouterProvider){
 
-}])
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('login', {
+      url: '/login',
+      templateUrl: './auth/loginView.html'
+    })
+    .state('signup', {
+      url: '/signup',
+      templateUrl: './auth/signupView.html'
+    })
+    .state('newsfeed', {
+      url: '/newsfeed/:groupname',
+      templateUrl: './newsFeed/newsFeedView.html'
+    });
+    .state('shoutmaker', {
+      url: '/shoutmaker',
+      templateUrl: './shoutMaker/shoutMakerView.html'
+    })
+    .state('groupmaker', {
+      url: '/groupmaker',
+      templateUrl: './groupMaker/groupCreationView.html'
+    })
+    .state('userprofile', {
+      url: '/userprofile/:username',
+      templateUrl: './userProfile/userProfileView.html'
+    });
+
+}]);
