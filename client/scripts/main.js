d1f319b5f1963d5d91d95e9899984010daee8833
Elizabeth Banalagay <zbanalagay@gmail.com>
2015-12-18 12:42:03 -0800

    [refactor] changed file paths

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -8,32 +8,33 @@ angular.module('shoutr',[
 ])
 .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
 
-  $urlRouterProvider.otherwise('shoutmaker');
+   $urlRouterProvider.otherwise('/shoutmaker');
 
   $stateProvider
+
     .state('login', {
       url: '/login',
-      templateUrl: './auth/loginView.html'
+      templateUrl: './scripts/auth/loginView.html'
     })
     .state('signup', {
       url: '/signup',
-      templateUrl: './auth/signupView.html'
+      templateUrl: './scripts/auth/signupView.html'
     })
     .state('newsfeed', {
       url: '/newsfeed/:groupname',
-      templateUrl: './newsFeed/newsFeedView.html'
+      templateUrl: './scripts/newsFeed/newsFeedView.html'
     })
     .state('shoutmaker', {
       url: '/shoutmaker',
-      templateUrl: './shoutMaker/shoutMakerView.html'
+      templateUrl: './scripts/shoutMaker/shoutMakerView.html'
     })
     .state('groupmaker', {
       url: '/groupmaker',
-      templateUrl: './groupMaker/groupCreationView.html'
+      templateUrl: './scripts/groupMaker/groupCreationView.html'
     })
     .state('userprofile', {
       url: '/userprofile/:username',
-      templateUrl: './userProfile/userProfileView.html'
+      templateUrl: './scripts/userProfile/userProfileView.html'
     });
 
 }]);
