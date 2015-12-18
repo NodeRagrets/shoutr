e1569982ef75481dd178e99bbb1ccf5fd6645ed2
tbergen1 <tom@servant.co>
2015-12-18 09:55:56 -0800

    [feature] add post functionality for shout controller

--- client/scripts/shoutMaker/shoutMakerController.js
+++ client/scripts/shoutMaker/shoutMakerController.js
@@ -2,4 +2,14 @@ angular.module('shoutr.shoutCreation', [])
 
 .controller('shoutCreationController', ['$scope', 'Shouts', function($scope, Shouts) {
   
+  $scope.shout = {
+    recipient: '',
+    title: '',
+    message: ''
+  };
+
+  $scope.postShout = function() {
+    Shouts.saveShout($scope.shout);
+  }
+
 }]);
