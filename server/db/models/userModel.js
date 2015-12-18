b0b814707ed95b7e1acec8f7bef94fc58fa0772e
Rachel Jenkins <rachel@Sinas-MacBook-Air-2.local>
2015-12-17 19:53:19 -0800

    [Feat] created Sequelize User model in userModel.js

---
+++ server/db/models/userModel.js
@@ -0,0 +1,24 @@
+var sequelize = require('../init.js');
+
+var User = sequelize.define('User', {
+	username: {
+		type: sequelize.STRING
+	},
+	password: {
+		type: sequelize.STRING 
+	}, 
+	//photo: {},
+	email: {
+		type: sequelize.STRING
+	}
+});
+
+User.sync({force: true}).then(function() {
+	return User.create({
+		username: "Tom",
+		password: "abc123",
+		email:"tom@tom.com"
+	});
+})
+
+module.exports = User;
