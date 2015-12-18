0d77882c3a5fa56555693a80c6b6b082d62201d7
Rachel Jenkins <rachel@Sinas-MacBook-Air-2.local>
2015-12-18 10:05:25 -0800

    [fix] updated to have exports in relationships.js

--- server/db/relationships.js
+++ server/db/relationships.js
@@ -3,7 +3,15 @@ var Group = require('./models/groupModel.js');
 var Shout = require('./models/shoutModel.js');
 
 User.hasMany(Shout, {as: 'creator'});
+// Shout.belongsTo(User);
+
 User.hasMany(Shout, {as: 'recipient'});
 Group.hasMany(User);
 User.belongsToMany(Group, {through: 'UserGroupJoin'});
 Group.belongsToMany(User, {through: 'UserGroupJoin'});
+
+module.exports = {
+	User: User,
+	Group: Group,
+	Shout: Shout
+}
