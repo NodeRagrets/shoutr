97ec0bbce0ece3ac67fa6fa36a494f7890a31e33
Elizabeth Banalagay <zbanalagay@gmail.com>
2015-12-24 10:58:30 -0500

    [structure] added pseudocode of post and get to handler files in routes directory

--- server/routes/shouts/shoutHandler.js
+++ server/routes/shouts/shoutHandler.js
@@ -1,3 +1,17 @@
-module.exports = {
+var express = require('express');
+var helpers = require('../helpers.js');
+var app = express();
 
+module.exports = {
+  app.get('/', function(req, res){
+    //find on the req where the groupName is stored
+    //call helpers.getShouts and pass it in
+    //.then with the data, retrieve all the shouts of that group in res.send
+  })
+  app.post('/', function(req, res){
+    //find on req where the shout data is
+    //call helpers.addShout and pass in req's shout data
+    //.then with that data, retrieve all the shouts in the res.send
+    res.send('Got a POST request, shoutHandler');
+  });
 }
