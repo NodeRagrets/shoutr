e97e6d01191a8e153a2821bf280dba75bd3a6a5e
Elizabeth Banalagay <zbanalagay@gmail.com>
2015-12-24 10:58:30 -0500

    [structure] added pseudocode of post and get to handler files in routes directory

--- server/routes/groups/groupHandler.js
+++ server/routes/groups/groupHandler.js
@@ -1,3 +1,10 @@
-module.exports = {
+var express = require('express');
+var app = express();
 
+module.exports = {
+  app.post('/', function(req, res){
+    //find on req where the Group data is
+    //call helpers.addGroup and pass in req's user data
+    //.then throw an error or res.send
+    res.send('Got a POST request, groupHandler');
 }
--- server/routes/users/userHandler.js
+++ server/routes/users/userHandler.js
@@ -1,3 +1,19 @@
-module.exports = {
+var express = require('express');
+var app = express();
 
+module.exports = {
+  app.get('/', function(req, res){
+
+    //find on the req where the userName is stored
+    //call helpers.getUser and pass it in
+    //.then with the data, send user in res.send
+
+  })
+  app.post('/', function(req, res){
+    //find on req where the user data is
+    //call helpers.addUser and pass in req's user data
+    //find on req where group data is
+    //call addUserToGroup with username and groupName as args
+    res.send('Got a POST request, userHandler');
+  });
 }
