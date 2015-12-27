d8a92001cf3def4645de7866b4fff86a048377d2
Elizabeth Banalagay <zbanalagay@gmail.com>
2015-12-27 15:03:03 -0500

    [style] fixed some style and semicolons in shoutHandlerjs

--- server/routes/shouts/shoutHandler.js
+++ server/routes/shouts/shoutHandler.js
@@ -12,11 +12,11 @@ module.exports = {
                  console.log('ERROR INSIDE GETSHOUTHANDLER', err);
                  res.send(404);
                } else{
-                 console.log('SUCCESS GOT A POST REQUEST, SHOUTHANDLER');
+                 console.log('SUCCESS GOT A GET REQUEST, SHOUTHANDLER');
                  res.send(200, resultData);
                  //TODO deal with how resultData is returned--- look at db cheatsheet
                }
-             })
+             });
     },
     postShoutHandler: function(req, res){
       var shoutData = req.body;//find the data or req.params.shouts
