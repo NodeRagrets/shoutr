d86eee46ef7d73c5898b87133ee82d4722d2805f
malekascha <malekascha@gmail.com>
2015-12-28 16:41:12 -0800

    [feat] Set up form submission for groupCreation in directory groupMaker

--- client/scripts/groupMaker/groupCreationController.js
+++ client/scripts/groupMaker/groupCreationController.js
@@ -2,5 +2,14 @@ angular.module('shoutr.groupCreation', [
 
 ])
 .controller('groupCreationController', ['$scope', function($scope){
+  $scope.groupCreated = false;
+  $scope.groupSubmission = function(){
+    if($scope.groupname){
+      //At this point, I will call the addGroup function in the relevant factory in services.js
+      //I am unsure about setting this up, so I am waiting until our meeting tomorrow
+    }
+    $scope.groupname = '';
+    $scope.groupCreated = true;
+  }
 
 }])
