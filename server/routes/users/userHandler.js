003292b240fa2b2dd00b24787198b5172984c40b
Rachel Jenkins <rachel@Sinas-MacBook-Air-2.local>
2015-12-29 15:14:56 -0800

    [feat] signup function hashes password and stores new user data in the DB

--- server/routes/users/userHandler.js
+++ server/routes/users/userHandler.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 var helpers = require('./../../db/helpers.js');
-
+var bcrypt = require('bcrypt-nodejs');
 
 module.exports = {
   login: function(req, res){
-    var userNameData = req.body //TODO find where the data is;
+    var userNameData = req.body;
     helpers.getUser(userNameData)
            .then( function(resultData){
             //  if(err){
@@ -19,26 +19,28 @@ module.exports = {
            })
            .catch( function(err){
              console.log(err, "ERROR INSIDE login");
-           });
+           }); 
 
   },
+  
   signup: function(req, res){
-    var userData = req.body //TODO find where the data is
-    var groupData = req.body //TODO find where the data is
 
-    helpers.addUserToGroup(userData, groupData)
-           .then(function(resultData){
-            //  if(err){
-            //    console.log('ERROR INSIDE POSTUSERHANDLER', err);
-            //    res.send(404);
-            //  } else{
-               console.log('SUCCESS POST A POST REQUEST, USERHANDLER');
-               res.status(200).send(resultData);
-            //  }
-          })
-          .catch( function(err){
-            console.log(err, "ERROR ISNIDE SIGNUP");
-          });
+    var userData = req.body;
+    // console.log("HERE IS THE USERDATA", userData);
 
+    bcrypt.hash(req.body.password, null, null, function(err, hash){
+      userData.password = hash;
+    });
+
+    var userPromise = helpers.addUser(userData);
+    
+    userPromise.then(function(resultData){
+         console.log('SUCCESSFUL POST REQUEST, USERHANDLER');
+         res.status(200).send(resultData);
+    })
+    .catch( function(err){
+      console.log(err, "ERROR INSIDE SIGNUP");
+    });
   }
+
 };
