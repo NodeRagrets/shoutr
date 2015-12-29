336c684b513ca913192da80c1b36476b8088229e
Rachel Jenkins <rachel@Sinas-MacBook-Air-2.local>
2015-12-29 14:57:43 -0800

    progress on signup function, authController.js

--- client/scripts/auth/authController.js
+++ client/scripts/auth/authController.js
@@ -1,6 +1,40 @@
-angular.module('shoutr.auth', [
+angular.module('shoutr.auth', [])
 
-])
-.controller('authController', [function(){
-  
+.controller('authController', ['$scope', '$window', '$location', 'Users', function($scope, $window, $location, Users){
+
+	$scope.signupData = {
+		user: {
+			email: '',
+			username: '',
+			password: ''
+		}
+	};
+
+	$scope.userSignedup = false;
+
+  $scope.signup = function() { 
+  	// TODO: add confirm password to view and logic to accomodate it here
+  	
+  	var signupResponse = Users.signup($scope.signupData.user);
+		
+		console.log("HERE IS SIGNUP RESPONSE", signupResponse);
+  	if (signupResponse.status === 200) {
+  		//TODO: begin session here $window.localStorage.setItem...
+  		// console.log('INSIDE THE SUCCESS 200 OF AUTHCONTROLLER');
+  		$location.path('/#/newsfeed');
+  		$scope.userSignedup = true;
+  	} else {
+  	  //Show error, passwords not matching.
+  	  //show error, account already exists
+  	  //div with ng-hide ng-model='error' 
+  	  $scope.error = signUpResponse.status;
+  	  console.log("HERE IS SCOPE ERROR", $scope.error);
+  	}
+  }
+
+
+
+  $scope.login = function() {
+  }
+
 }])
