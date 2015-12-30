dfdb1021eb87cea31fd0d2cd29aeca88707fc837
Rachel Jenkins <rachel@Sinas-MacBook-Air-2.local>
2015-12-29 16:48:41 -0800

    [feat] built out login function that compares the password of someone logging in, userHandler.js

--- server/routes/users/userHandler.js
+++ server/routes/users/userHandler.js
@@ -4,25 +4,34 @@ var helpers = require('./../../db/helpers.js');
 var bcrypt = require('bcrypt-nodejs');
 
 module.exports = {
+
+
   login: function(req, res){
-    var userNameData = req.body;
-    helpers.getUser(userNameData)
-           .then( function(resultData){
-            //  if(err){
-            //    console.log('ERROR INSIDE GETUSERHANDLER', err);
-            //    res.send(404);
-            //  } else{
-               console.log('SUCCESS GOT A GET REQUEST, USERHANDLER');
-               res.status(200).send(resultData);
 
-            //  }
-           })
-           .catch( function(err){
-             console.log(err, "ERROR INSIDE login");
-           }); 
+    var loginData = req.body;
+    var loginPromise = helpers.getUser(loginData.username);
+    console.log("BOOYAH HERE IS DATA FROM LOGIN FN", req.body);
+    
+    loginPromise.then(function(resultData){
+      // console.log("HERE IS RESULTDATA", resultData);
+      bcrypt.compare(loginData.password, resultData.password, function(err, result) {
+        if (result === true) {
+          console.log('PASSWORDS ARE THE SAME, USER MAY PROCEED!');
+          res.status(200).send(resultData);
+        } else {
 
+        }
+      })
+
+    })
+    .catch(function(err){
+      console.log(err, "ERROR INSIDE LOGIN");
+    }); 
   },
   
+
+
+
   signup: function(req, res){
 
     var userData = req.body;
@@ -35,8 +44,8 @@ module.exports = {
     var userPromise = helpers.addUser(userData);
     
     userPromise.then(function(resultData){
-         console.log('SUCCESSFUL POST REQUEST, USERHANDLER');
-         res.status(200).send(resultData);
+      console.log('SUCCESSFUL POST REQUEST, USERHANDLER');
+      res.status(200).send(resultData);
     })
     .catch( function(err){
       console.log(err, "ERROR INSIDE SIGNUP");
