5e48dbb1efa10073da3d0b3437067632ecc66e86
Rachel Jenkins <rachel@Sinas-MacBook-Air-2.local>
2015-12-29 16:41:51 -0800

    [feat] added login function, authController.js

--- client/scripts/auth/authController.js
+++ client/scripts/auth/authController.js
@@ -4,37 +4,58 @@ angular.module('shoutr.auth', [])
 
 	$scope.signupData = {
 		user: {
-			email: '',
-			username: '',
-			password: ''
+			email: 'asd@asd.confirm',
+			username: 'asdasd',
+			password: 'asdsdsdsd'
+		}
+	};
+
+	$scope.loginData = {
+		user: {
+			username: 'woooohaa',
+			password: 'aekuhwa'
 		}
 	};
 
 	$scope.userSignedup = false;
 
   $scope.signup = function() { 
-  	// TODO: add confirm password to view and logic to accomodate it here
+  	// TODO: add confirm password to signup view, and logic to accomodate it here
   	
-  	var signupResponse = Users.signup($scope.signupData.user);
-		
-		console.log("HERE IS SIGNUP RESPONSE", signupResponse);
-  	if (signupResponse.status === 200) {
-  		//TODO: begin session here $window.localStorage.setItem...
-  		// console.log('INSIDE THE SUCCESS 200 OF AUTHCONTROLLER');
-  		$location.path('/#/newsfeed');
-  		$scope.userSignedup = true;
-  	} else {
-  	  //Show error, passwords not matching.
-  	  //show error, account already exists
-  	  //div with ng-hide ng-model='error' 
-  	  $scope.error = signUpResponse.status;
-  	  console.log("HERE IS SCOPE ERROR", $scope.error);
-  	}
+  	Users.signup($scope.signupData.user).then(function(response){
+  			if (response.status === 200) {
+		  		// TODO: begin session here $window.localStorage.setItem
+		  		console.log("response is", response);
+		  		$scope.userSignedup = true;
+		  		$location.path('/#/newsfeed');
+  			} else {
+  				//TODO: display appropriate error to user
+  				$scope.error = response.status;
+  	  		console.log("HERE IS SCOPE ERROR", $scope.error);
+  			}
+  	});
   }
 
 
 
   $scope.login = function() {
+  	console.log("here's the login data", $scope.loginData.user);
+  	Users.login($scope.loginData.user).then(function(response) {
+  		if (response.status === 200) {
+  			$location.path('/#/newsfeed');
+  		} else {
+  			//TODO: figure out how to handle this case 
+  			console.log('problem with login');
+  		}
+  	})
   }
 
 }])
+
+
+
+
+
+
+
+
