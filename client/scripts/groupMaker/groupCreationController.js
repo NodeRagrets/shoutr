5b32a268cba359d84ae0a48a9e5b2fa58e6971f0
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-01 09:38:36 -0800

    [refactor] fixed variable name of groupName in groupmaker folder client side

--- client/scripts/groupMaker/groupCreationController.js
+++ client/scripts/groupMaker/groupCreationController.js
@@ -4,9 +4,9 @@ angular.module('shoutr.groupCreation', [
 .controller('groupCreationController', ['$scope', 'Groups', function($scope, Groups){
   $scope.groupCreated = false;
   $scope.data = {};
-  $scope.data['groupname']= '';
+  $scope.data['groupName']= '';
   $scope.groupSubmission = function(){
-    if($scope.data.groupname){
+    if($scope.data.groupName){
       // $scope.data['groupname'] = $scope.groupname;
       Groups.createGroup($scope.data);
       // $scope.groupname = '';
