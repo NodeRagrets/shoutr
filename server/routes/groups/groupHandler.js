21ff65a4726d69c2ffaae3c50b02d8ed0155120c
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-02 21:44:35 -0800

    [refactor] able to add users to new group in groupHandlerjs

--- server/routes/groups/groupHandler.js
+++ server/routes/groups/groupHandler.js
@@ -19,31 +19,30 @@ module.exports = {
     //
     // },
     saveGroup: function(req, res){
-      console.log(req.body, 'SAVEGROUP');
+
       var groupData = {};
       groupData['groupName'] = req.body.groupName;
       var groupNameForAddUsers = req.body.groupName;
-      // console.log(typeof req.body)
-      var userString = req.body.username;
-      var userArray = userString.split(',');
-      console.log(userArray, "USERARRAY");
+      // console.log(groupData, "GROUPDATA")
+      var userArray = req.body.usersArray;
+      // console.log(userArray, "USERARRAY");
       helpers.addGroup(groupData)
              .then( function(resultData){
                  console.log('SUCCESS GOT A POST REQUEST, SAVEGROUP');
                  for(var i = 0; i < userArray.length; i++){
+                  //  console.log("CURRENTELEMENT USERARRAY", userArray[i])
                    helpers.addUserToGroup(userArray[i], groupNameForAddUsers)
                           .then(function(results){
-                            console.log("RESULTS IN FORLOOP", results, "RESULTS IN FORLOOP")
                             console.log('SUCCESSFULLY ADDED USER');
                           })
                           .catch( function(err){
-                            console.log('ERROR ADDING USER');
+                            console.log('ERROR ADDING USER', err);
                           })
                  }
                  res.status(200).send(resultData);
              })
              .catch( function(err){
-               console.log('ERROR INSIDE SAVEGROUP');
+               console.log('ERROR INSIDE SAVEGROUP', err);
              });
 
     }
