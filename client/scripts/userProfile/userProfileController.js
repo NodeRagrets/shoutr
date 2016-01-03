930f6ca8c46f512adb59644c24f576aa7aa2fc00
Rachel Jenkins <jenkinsr07@gmail.com>
2016-01-03 13:31:49 -0800

    [feat] progress on getUser function, userProfileController.js

--- client/scripts/userProfile/userProfileController.js
+++ client/scripts/userProfile/userProfileController.js
@@ -4,16 +4,22 @@ angular.module('shoutr.userProfile', [])
 
   $scope.data = {};
 	$scope.data.username = $stateParams.username;
+	$scope.data.shoutsToUser;
+	$scope.data.shoutsFromUser;
 
-	console.log($scope.data.username);
-
+	// console.log($scope.data.username);
+	
+	//THIS NEEDS SHOUTS TO USERS TO BE TESTED
   $scope.getUser = function(username) {
     Users.pullUser(username).then(function(response) {
-
       $scope.data.user = response;
+      $scope.data.shoutsToUser = response.shoutsReceived;
+      console.log(response.shoutsReceived);
     });
   }
 
   $scope.getUser($scope.data.username);
   
 }]);
+
+
