704f15ec68c28eca4fe8a0efaa92af4413aefef2
Rachel Jenkins <jenkinsr07@gmail.com>
2016-01-03 22:47:22 -0800

    [feat] uploadpic function working, userprofilecontroller.js

--- client/scripts/userProfile/userProfileController.js
+++ client/scripts/userProfile/userProfileController.js
@@ -16,7 +16,7 @@ angular.module('shoutr.userProfile', ['ngFileUpload'])
   $scope.getUser = function(username) {
     Users.pullUser(username).then(function(response) {
       $scope.data.user = response;
-      $scope.data.shoutsToUser = response.shoutsReceived;
+      // $scope.data.shoutsToUser = response.shoutsReceived;
       // console.log(response.shoutsReceived);
     });
   }
@@ -24,16 +24,23 @@ angular.module('shoutr.userProfile', ['ngFileUpload'])
 
   $scope.uploadPic = function(file) { 
     $scope.picFile = file;
-    
-    // Upload.base64DataUrl(file
-    Users.storeProfilePic(file).then(function(response) {
-      file.result = response.data;
-      console.log("here is response", response);
+    console.log("USERNAME IS HERE", $scope.data.username);
+
+    var userDataWithFile = {
+      'blobUrl': file.$ngfBlobUrl,
+      'username': $scope.data.username
+    };
+
+    Users.storeProfilePic(userDataWithFile)
+      .then(function(response) {
+        console.log(response);
+      // file.result = response.data;
+      // console.log("here is THE URL", response.config.data.pic.$ngfBlobUrl);
       // if (response.status > 0) {
       //   $scope.errorMsg = response.status + ': ' + response.data;
       // }
       // file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-    });
+      });
   }
 
   $scope.getUser($scope.data.username);
