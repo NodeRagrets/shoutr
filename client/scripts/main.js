b5fdb1fce291f6cc27833a3d9d5c84771c8e7e50
Rachel Jenkins <jenkinsr07@gmail.com>
2016-01-03 19:43:20 -0800

    [feat] required ngFileUpload for profile pics, main.js

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -8,7 +8,8 @@ angular.module('shoutr',[
   'shoutr.newsFeed',
   'shoutr.userProfile',
   'shoutr.services',
-  'RDash'
+  'RDash',
+  'ngFileUpload'
 ])
 .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
 
