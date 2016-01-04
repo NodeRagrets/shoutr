335d8c609c5e99b3162adc17d2caefd86624d250
tbergen1 <tom@servant.co>
2016-01-04 04:28:10 -0800

    [refactor] replace sessions handlers with jwt handlers in userHandler.js

--- server/routes/users/userHandler.js
+++ server/routes/users/userHandler.js
@@ -3,81 +3,56 @@ var app = express();
 var helpers = require('./../../db/helpers.js');
 var bcrypt = require('bcrypt-nodejs');
 var utils = require('./../../config/utils.js');
+var SALT_WORK_FACTOR = 10;
 
 module.exports = {
 
+  authenticate: function(req, res) {
+    console.log(req.headers);
+    var username = req.body.username;
+    var password = req.body.password;
 
-  login: function(req, res) {
+    if(!username || !password) {
+      return res.status(401).send('Username or password not present');
+    }
 
-    var loginData = req.body;
-    var loginPromise = helpers.getUser(loginData.username);
-    
-    loginPromise.then(function(resultData){
-      //if the call to getUser does not resolve to an error, the passwords are compared 
-      bcrypt.compare(loginData.password, resultData.password, function(err, result) {
-        if (result === true) {
-          // Regenerate session when signing in to prevent fixation
-          utils.createSession(req, res, resultData);
-
-        } else {
-          res.status(422).send(err);
-        }
+    helpers.getUser(username)
+      .then(function(user) {
+        bcrypt.compare(password, user.password, function(error, result) {
+          if(result) {
+            return res.status(200).json({user: user, token: utils.issueToken(user)});
+          }
+          if(error) {
+            return res.status(401).send(error)
+          }
+        });
+      })
+      .catch(function(error) {
+        return res.status(401).send(error);
       })
 
-    }) //if the call to getUser resolves to an error, we know the user does not exist and send them 404 
-    .catch(function(err){
-      res.status(404).send(err);
-    }); 
-  
   },
 
-
-
-  signup: function(req, res) {
-
-    var userData = req.body;
-
-    bcrypt.hash(req.body.password, null, null, function(err, hash){
-      userData.password = hash;
+  register: function(req, res) {
+    var user = req.body;
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(error, salt) {
+      if(error) {
+        return res.status(401).send(error)
+      }
+      bcrypt.hash(user.password, null, null, function(error, hash) {
+        if(error) {
+          throw error;
+        }
+        user.password = hash;
+        helpers.addUser(user)
+          .then(function(user) {
+            return res.status(200).send({user: user, token: utils.issueToken(user)});
+          })
+          .catch(function(error) {
+            return res.status(401).send(error);
+          })
+      });
     });
-
-    var userPromise = helpers.addUser(userData);
-    
-    userPromise.then(function(resultData){
-      utils.createSession(req, res, resultData);
-    })
-    .catch( function(err){
-      res.status(409).send(err);
-    });
-  
-  },
-
-
-
-  profile: function(req, res) {
-    var username = req.query.username;
-    // console.log("HERE IS REQ FROM PROFILE FN:", req.query.username);
-    var usernamePromise = helpers.getUser(username);
-
-    usernamePromise.then(function(resultData) {
-      res.status(200).send(resultData);
-    })
-    .catch( function(err){
-      res.status(404).send(err);
-    });
-  
-  },
-
-
- 
-  logout: function(req, res) {
-  // destroy the user's session to log them out
-    req.session.destroy(function(){
-      console.log('successful user session destruction!');
-      res.status(200).send("successful log out!");
-    });
-  
   }
-
-
+  
 };
