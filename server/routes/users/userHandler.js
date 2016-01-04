80d2e0012835c237b8eb1bb1480b2f7c54f9515e
Rachel Jenkins <jenkinsr07@gmail.com>
2016-01-04 10:28:57 -0800

    [fix] fixed merge conflict, userHandler.js

--- server/routes/users/userHandler.js
+++ server/routes/users/userHandler.js
@@ -57,7 +57,6 @@ module.exports = {
 
   profile: function(req, res) {
     var username = req.query.username;
-    // console.log("HERE IS REQ FROM PROFILE FN:", req.query.username);
     var usernamePromise = helpers.getUser(username);
     var recipientShoutsPromise;
     usernamePromise
@@ -67,18 +66,25 @@ module.exports = {
         recipientShoutsPromise
           .then(function (shoutsArray) {
             //CURRENTLY, SHOUTSARRAY IS UNDEFINED. DOES THIS CHANGE ONCE THE DB CONTAINS SHOUTS RECEIVED BY A GIVEN USER?
-            console.log("HERE IS SHOUTSARRAY", shoutsArray);
+            // console.log("HERE IS SHOUTSARRAY", shoutsArray);
             resultData.shoutsReceived = shoutsArray;
-            console.log("HERE IS RESULTDATA INSIDE PROFILE FN, USERHANDLER", resultData);
+            // console.log("HERE IS RESULTDATA INSIDE PROFILE FN, USERHANDLER", resultData);
             //ADD SHOUTSSENT ARRAY HERE 
             res.status(200).send(resultData);
           }); 
       })
     .catch( function(err){
       res.status(404).send(err);
-    });
-  }
+    }); 
+  },
 
-  
+  storeProfilePic: function(req, res) {
+    var addPicPromise = helpers.addProfilePicToUser(req.body);
+
+    addPicPromise.then(function(resultData) {
+      //
+      res.status(200).send("SUCCESS, DATA STORED", resultData);
+    });
+  },
   
 };
