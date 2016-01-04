40b6319bdb5c5dbcfed20ad95f292fb61237b902
Rachel Jenkins <jenkinsr07@gmail.com>
2016-01-03 19:45:08 -0800

    [feat] added uploadPic function, userProfileController.js

--- client/scripts/userProfile/userProfileController.js
+++ client/scripts/userProfile/userProfileController.js
@@ -1,20 +1,38 @@
-angular.module('shoutr.userProfile', [])
+angular.module('shoutr.userProfile', ['ngFileUpload'])
 
-.controller('userProfileController', ['$scope', 'Users', '$stateParams', function($scope, Users, $stateParams) {
+.controller('userProfileController', ['$scope', 'Users', '$stateParams', 'Upload', '$timeout', function($scope, Users, $stateParams, Upload, $timeout) {
 
   $scope.data = {};
 	$scope.data.username = $stateParams.username;
 	$scope.data.shoutsToUser;
 	$scope.data.shoutsFromUser;
+  
+  $scope.picFile;
 
-	// console.log($scope.data.username);
+
+	console.log("HERE IS USERNAME", $scope.data.username);
 	
-	//THIS NEEDS SHOUTS TO USERS TO BE TESTED
+
   $scope.getUser = function(username) {
     Users.pullUser(username).then(function(response) {
       $scope.data.user = response;
       $scope.data.shoutsToUser = response.shoutsReceived;
-      console.log(response.shoutsReceived);
+      // console.log(response.shoutsReceived);
+    });
+  }
+
+
+  $scope.uploadPic = function(file) { 
+    $scope.picFile = file;
+    
+    // Upload.base64DataUrl(file
+    Users.storeProfilePic(file).then(function(response) {
+      file.result = response.data;
+      console.log("here is response", response);
+      // if (response.status > 0) {
+      //   $scope.errorMsg = response.status + ': ' + response.data;
+      // }
+      // file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
     });
   }
 
