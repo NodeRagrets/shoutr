bcdd693b621f659e7f18cd1064631fd329aff848
Rachel Jenkins <jenkinsr07@gmail.com>
2016-01-04 18:10:41 -0800

    [fix] finally got the profile working again wooooot, scripts/userProfile

--- client/scripts/userProfile/userProfileController.js
+++ client/scripts/userProfile/userProfileController.js
@@ -1,28 +1,27 @@
-angular.module('shoutr.userProfile', [])
+angular.module('shoutr.userProfile', ['ngFileUpload'])
 
-.controller('userProfileController', ['$scope', 'Users', '$stateParams', '$timeout', 'PicData', function($scope, Users, $stateParams, $timeout, PicData) {
+.controller('userProfileController', ['$scope', 'Users', '$stateParams', '$timeout', 'Upload', 'PicData', function($scope, Users, $stateParams, $timeout, Upload, PicData) {
 
 
   $scope.data = {};
 	$scope.data.username;
-	$scope.data.shoutsToUser;
-	$scope.data.shoutsFromUser;
+  $scope.data.email;
   
-  $scope.picFile;
+
 
 
   $scope.getUser = function() {
     Users.pullUser().then(function(response) {
-      $scope.data.username = response.token.username;
-      console.log("HERE IS USERNAME", $scope.data.username);
+      $scope.data.username = response.data.username;
+      $scope.data.email = response.data.email;
     });
   }
 
 
   $scope.uploadPic = function(file) { 
-    $scope.picFile.imgBlobUrl = file.$ngfBlobUrl;
-    console.log("USERNAME IS HERE", $scope.data.username);
+    $scope.picFile = file.$ngfBlobUrl;    
 
+    //this is the PicData factory, which is the way of sharing data with the main controller. it gets injected there, as well, to post the uploaded image to the dashboard view.
     $scope.PicData.profilePic = file.$ngfBlobUrl;
 
     var userDataWithFile = {
@@ -33,7 +32,6 @@ angular.module('shoutr.userProfile', [])
     Users.storeProfilePic(userDataWithFile)
       .then(function(response) {
         console.log("hello, response", response);
-        $scope.PicData.profilePic = response.config.data.blobUrl;
       });
   }
 
